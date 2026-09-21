56d6973af7806fdd27a6c11fcbd697f6e6cdf1ef
niranjannagumalli <niranjannagumalli@gmail.com>
2026-09-21 14:10:44 +0530

    plot the measured photons after doing a sweep

--- main.py
+++ main.py
@@ -1,24 +1,65 @@
 import pyvisa
-import streamlit as st
+import time
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-# st.set_page_config(layout="wide")
-# st.title("Fake Laser: Demonstration")
-
-#initiliazing the simulator using the laser.yaml file
-# @st.cache_resource
-def connect_instrument():
+def run_experiment():
     rm = pyvisa.ResourceManager('laser.yaml@sim')
     inst = rm.open_resource('ASRL1::INSTR')
     inst.write_termination = '\n'
     inst.read_termination = '\n'
+    print(f"Connected to: {inst.query('*IDN?')}")   
+
+    voltages = np.linspace(0.0, 5.0, 25)
+    measured_photons = []
+
+    plt.ion()
+    fig, ax = plt.subplots(figsize = (8,5))
+    line, = ax.plot([], [], 'bo-', label='Live Data')
+    ax.set_xlim(0, 5)
+    ax.set_ylim(0, 1600)
+    ax.set_xlabel("Applied Voltage (V)")
+    ax.set_ylabel("Simulated Photon Counts")
+    ax.set_title("Real-Time Photonic Measurement")
+    ax.grid(True, alpha=0.3)
+
+    print("-"*60)
+    print("Initiating voltage sweep")
+    print("-"*60)
+
+    # now we slowly set the voltage and read the voltage back(ik its dumb) and keep increasing. 
+    # for everytime we read teh value back, we calculate the number of photons which could have been emitted + some noise. 
+
+    for voltage in voltages:
+        #set the voltage
+        inst.write(f"VOLT {voltage:.3f}")
+
+        # we simulate hardware delay, XD
+        time.sleep(0.1)
+
+        #now we read it back
+        read_voltage = float(inst.query("VOLT?"))
+
+        #simulate a real laser which is linear above a certain voltage. 
+        if read_voltage < 2.0:
+            #somebackground noise
+            photons = np.random.normal(10,3)
+        else:
+            photons = ((read_voltage - 2.0)* 500) + np.random.normal(0,20)
+
+        measured_photons.append(photons)
+
+        line.set_xdata(voltages[:len(measured_photons)])
+        line.set_ydata(measured_photons)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
     
+    plt.ioff()
+    plt.show()
+
+
     return inst
 
-instrument = connect_instrument()
+instrument = run_experiment()
 
-# st.success(f"Connected to: {instrument.query('*IDN?')}")
-print(f"Connected to: {instrument.query('*IDN?')}")
-
-readVoltage = float(instrument.query("VOLT?"))
-print(readVoltage)
