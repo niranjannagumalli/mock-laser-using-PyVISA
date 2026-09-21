8480282c57c2009d4703aa9ddeb4f8e21dade1a1
niranjannagumalli <niranjannagumalli@gmail.com>
2026-09-21 13:34:09 +0530

    read from the fake machine successfully

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import pyvisa
+import streamlit as st
 
-rm = pyvisa.ResourceManager('@sim')
-rm.list_resources()
+
+# st.set_page_config(layout="wide")
+# st.title("Fake Laser: Demonstration")
+
+#initiliazing the simulator using the laser.yaml file
+# @st.cache_resource
+def connect_instrument():
+    rm = pyvisa.ResourceManager('laser.yaml@sim')
+    inst = rm.open_resource('ASRL1::INSTR')
+    inst.write_termination = '\n'
+    inst.read_termination = '\n'
+    
+    return inst
+
+instrument = connect_instrument()
+
+# st.success(f"Connected to: {instrument.query('*IDN?')}")
+print(f"Connected to: {instrument.query('*IDN?')}")
+
+readVoltage = float(instrument.query("VOLT?"))
+print(readVoltage)
